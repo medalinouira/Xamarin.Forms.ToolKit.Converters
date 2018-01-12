8566616385be467f28a91feb00b1b654f7f62a86
Mohamed Ali NOUIRA <mohamedalinouira@gmail.com>
2018-01-12 23:34:16 +0100

    Add project files.

---
+++ Xamarin.Forms.ToolKit.Converters/SelectedItemConverter.cs
@@ -0,0 +1,26 @@
+﻿/// Mohamed Ali NOUIRA
+/// http://www.sweetmit.com
+/// https://github.com/medalinouira
+/// Copyright © Mohamed Ali NOUIRA. All rights reserved.
+using System;
+using System.Globalization;
+
+namespace Xamarin.Forms.ToolKit.Converters
+{
+    public class SelectedItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var eventArgs = value as ItemTappedEventArgs;
+            if (eventArgs == null)
+                return null;
+
+            return eventArgs.Item;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
